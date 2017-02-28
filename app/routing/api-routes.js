94144dbfc79434fa97a87843528e18beac85194c
simishr <smriti.shrestha271@gmail.com>
2017-02-28 15:20:05 -0500

    final commit

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -11,4 +11,26 @@ module.exports = function (app) {
 	app.get("/api/waitlist", function (req, res) {
 		res.json(waitListData);
 	});
+
+	app.post("/api/tables", function (req, res) {
+
+		if(tableData.length < 5) {
+
+			tableData.push(req.body);
+			res.json(true);
+
+		} else {
+
+			waitListData.push(req.body);
+			res.json(false);
+		}
+	});
+
+	app.post("/api/clear", function() {
+		tableData = [];
+		waitListData = [];
+
+		console.log(tableData);
+		console.log(waitListData);
+	})
 }
