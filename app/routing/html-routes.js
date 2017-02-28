22a6ef5570bd441b87727e4850ce39122082eae0
simishr <smriti.shrestha271@gmail.com>
2017-02-27 23:07:18 -0500

    initial upload

---
+++ app/routing/html-routes.js
@@ -0,0 +1,18 @@
+// this will help direct the user..when user clicks on the links, gives back specific links..
+var path = require("path");
+
+module.exports = function (app) {
+
+	// viewed at http://localhost:8080
+	app.get('/tables', function(req, res) {
+	    res.sendFile(path.join(__dirname + '/../public/tables.html'));
+	});
+
+	app.get('/reserve', function(req, res) {
+	    res.sendFile(path.join(__dirname + '/../public/reserve.html'));
+	});
+
+	app.get('/', function(req, res) {
+	    res.sendFile(path.join(__dirname + '/../home.html'));
+	});
+}
